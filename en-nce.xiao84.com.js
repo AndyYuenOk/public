fa27df75fede72c24ad070d58e77bf5e8f2d6d05
Andy <2444909880@qq.com>
2023-11-27 23:36:29 +0800

    Create en-nce.xiao84.com.js

---
+++ en-nce.xiao84.com.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         巴士英语网
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://en-nce.xiao84.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle('html,body{min-width:unset}.fw{width:unset}.mframe .c_right{display:none}.mframe .c_body{width:unset;margin:10px}.mframe .c_body_in{margin:unset}');
+})();
