a6d78d74d3246282ed2f85a9892dba8f298976b2
Andy <2444909880@qq.com>
2025-11-30 14:03:46 +0800

    Implement sniffer configuration in main function
    Added sniffer configuration to the main function.

--- clashextendscript.js
+++ clashextendscript.js
@@ -1,96 +1,105 @@
 // Define main function (script entry)
 
 function main(config, profileName) {
-  // config['proxy-groups'].splice(
-  //   config['proxy-groups'].findIndex(group => group.name === 'AdBlock'),
-  //   0,
-  //   {
-  //     name: 'AntiAd',
-  //     type: 'select',
-  //     proxies: ['REJECT', 'DIRECT', 'Proxy']
-  //   }
-  // );
-  // config["rule-providers"].AntiAd = {
-  //   type: 'http',
-  //   url: "https://gh-proxy.org/https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-clash.yaml",
-  //   interval: 86400,
-  //   proxy: 'DIRECT',
-  //   behavior: 'domain'
-  // };
-  // config.rules.unshift('RULE-SET,AntiAd,AntiAd,REJECT');
+    config.sniffer = {
+        enable: true,
+        sniff: {
+            HTTP: { ports: [80, 8080, 8081, 8082, 8083, 8084, 8085, 8086, 8087, 8088] },
+            TLS: { ports: [443, 8443] },
+            QUIC: { ports: [443, 8443] }
+        }
+    };
 
-  // config['proxy-groups'].splice(
-  //   config['proxy-groups'].findIndex(group => group.name === 'AdBlock'),
-  //   0,
-  //   {
-  //     name: 'AdBlockFiltersLite',
-  //     type: 'select',
-  //     proxies: ['REJECT', 'DIRECT', 'Proxy']
-  //   }
-  // );
-  // config["rule-providers"]['AdBlockFiltersLite'] = {
-  //   type: 'http',
-  //   url: "https://gcore.jsdelivr.net/gh/217heidai/adblockfilters@main/rules/adblockmihomolite.yaml",
-  //   interval: 86400,
-  //   proxy: 'DIRECT',
-  //   behavior: 'domain'
-  // };
-  // config.rules.unshift('RULE-SET,AdBlockFiltersLite,AdBlockFiltersLite,REJECT');
+    // config['proxy-groups'].splice(
+    //   config['proxy-groups'].findIndex(group => group.name === 'AdBlock'),
+    //   0,
+    //   {
+    //     name: 'AntiAd',
+    //     type: 'select',
+    //     proxies: ['REJECT', 'DIRECT', 'Proxy']
+    //   }
+    // );
+    // config["rule-providers"].AntiAd = {
+    //   type: 'http',
+    //   url: "https://gh-proxy.org/https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-clash.yaml",
+    //   interval: 86400,
+    //   proxy: 'DIRECT',
+    //   behavior: 'domain'
+    // };
+    // config.rules.unshift('RULE-SET,AntiAd,AntiAd,REJECT');
 
-  // config['proxy-groups'].splice(
-  //   config['proxy-groups'].findIndex(group => group.name === 'AdBlock'),
-  //   0,
-  //   {
-  //     name: 'AdBlockFilters',
-  //     type: 'select',
-  //     proxies: ['REJECT', 'DIRECT', 'Proxy']
-  //   }
-  // );
-  // config["rule-providers"]['AdBlockFilters'] = {
-  //   type: 'http',
-  //   url: "https://gcore.jsdelivr.net/gh/217heidai/adblockfilters@main/rules/adblockmihomo.yaml",
-  //   interval: 86400,
-  //   proxy: 'DIRECT',
-  //   behavior: 'domain'
-  // };
-  // config.rules.unshift('RULE-SET,AdBlockFilters,AdBlockFilters,REJECT');
+    // config['proxy-groups'].splice(
+    //   config['proxy-groups'].findIndex(group => group.name === 'AdBlock'),
+    //   0,
+    //   {
+    //     name: 'AdBlockFiltersLite',
+    //     type: 'select',
+    //     proxies: ['REJECT', 'DIRECT', 'Proxy']
+    //   }
+    // );
+    // config["rule-providers"]['AdBlockFiltersLite'] = {
+    //   type: 'http',
+    //   url: "https://gcore.jsdelivr.net/gh/217heidai/adblockfilters@main/rules/adblockmihomolite.yaml",
+    //   interval: 86400,
+    //   proxy: 'DIRECT',
+    //   behavior: 'domain'
+    // };
+    // config.rules.unshift('RULE-SET,AdBlockFiltersLite,AdBlockFiltersLite,REJECT');
 
-  const groupNames = [
-    'AdBlock',
-    'HTTPDNS',
-    'Netflix',
-    'Disney Plus',
-    'YouTube',
-    'Max',
-    'Spotify',
-    'CN Mainland TV',
-    'Asian TV',
-    'Global TV',
-    'Apple',
-    'Apple TV',
-    'Telegram',
-    'Google FCM',
-    'Crypto',
-    'Discord',
-    'AI Suite',
-    'PayPal',
-    'Scholar',
-    'Speedtest',
-    'Steam',
-    'TikTok',
-    'miHoYo'
-  ];
+    // config['proxy-groups'].splice(
+    //   config['proxy-groups'].findIndex(group => group.name === 'AdBlock'),
+    //   0,
+    //   {
+    //     name: 'AdBlockFilters',
+    //     type: 'select',
+    //     proxies: ['REJECT', 'DIRECT', 'Proxy']
+    //   }
+    // );
+    // config["rule-providers"]['AdBlockFilters'] = {
+    //   type: 'http',
+    //   url: "https://gcore.jsdelivr.net/gh/217heidai/adblockfilters@main/rules/adblockmihomo.yaml",
+    //   interval: 86400,
+    //   proxy: 'DIRECT',
+    //   behavior: 'domain'
+    // };
+    // config.rules.unshift('RULE-SET,AdBlockFilters,AdBlockFilters,REJECT');
 
-  config.rules = config.rules.filter(rule => !groupNames.find(name => RegExp(`,${name}$`).test(rule)));
+    const groupNames = [
+        'AdBlock',
+        'HTTPDNS',
+        'Netflix',
+        'Disney Plus',
+        'YouTube',
+        'Max',
+        'Spotify',
+        'CN Mainland TV',
+        'Asian TV',
+        'Global TV',
+        'Apple',
+        'Apple TV',
+        'Telegram',
+        'Google FCM',
+        'Crypto',
+        'Discord',
+        'AI Suite',
+        'PayPal',
+        'Scholar',
+        'Speedtest',
+        'Steam',
+        'TikTok',
+        'miHoYo'
+    ];
 
-  config["proxy-groups"] = config["proxy-groups"].filter(group => !groupNames.find(groupName => groupName == group.name));
+    config.rules = config.rules.filter(rule => !groupNames.find(name => RegExp(`,${name}$`).test(rule)));
 
-  // config['rule-providers'] = Object.fromEntries(Object.entries(config['rule-providers']).filter(([name, group]) => {
-  //   return !groupNames.includes(name);
-  // }));
+    config["proxy-groups"] = config["proxy-groups"].filter(group => !groupNames.find(groupName => groupName == group.name));
 
-  // config.script.code = config.script.code.replace("{\n", "{\n        'AdBlock DNS Lite': 'AdBlock DNS Lite',\n        ");
-  // config.script.code = '';
+    // config['rule-providers'] = Object.fromEntries(Object.entries(config['rule-providers']).filter(([name, group]) => {
+    //   return !groupNames.includes(name);
+    // }));
 
-  return config;
+    // config.script.code = config.script.code.replace("{\n", "{\n        'AdBlock DNS Lite': 'AdBlock DNS Lite',\n        ");
+    // config.script.code = '';
+
+    return config;
 }
