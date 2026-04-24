8652ef2b19c0c616e0432ec601d879e7b47fec5d
Andy <2444909880@qq.com>
2026-04-24 18:59:06 +0800

    -

--- sub-store-rename-custom.js
+++ sub-store-rename-custom.js
@@ -4,4 +4,5 @@ $server.name = $server.name
   .replace(/Chinanet/, "CT")
   .replace("Amazon.com, Inc.", "AMZ")
   .replace("Cloudflare, Inc.", "CF")
+  .replace("TECHNOLOGY CO., LIMITED", "")
   .replace(/(.+?)\s*((\ud83c[\udde6-\uddff]){2})\s*(.+)$/, "$2 $1 $4");
