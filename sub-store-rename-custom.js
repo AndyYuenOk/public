2bc15edb9442c94c66a7b24dec0c61533d3404a1
Andy <2444909880@qq.com>
2026-04-24 19:10:35 +0800

    -

--- sub-store-rename-custom.js
+++ sub-store-rename-custom.js
@@ -5,4 +5,5 @@ $server.name = $server.name
   .replace("Amazon.com, Inc.", "AMZ")
   .replace("Cloudflare, Inc.", "CF")
   .replace("TECHNOLOGY CO., LIMITED", "")
-  .replace(/(.+?)\s*((\ud83c[\udde6-\uddff]){2})\s*(.+)$/, "$2 $1 $4");
+  .replace(/(.+?)\s*((\ud83c[\udde6-\uddff]){2})\s*(.+)$/, "$2 $1 $4")
+  .replace("  ", " ");
