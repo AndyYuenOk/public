e50433128b820e511f045a0a22d9980fcd683b90
Andy <2444909880@qq.com>
2026-04-24 14:49:41 +0800

    -

---
+++ SubStoreRenameAso.js
@@ -0,0 +1,7 @@
+$server.name = $server.name
+  .replace(/China Mobile.+?corporation/, "CM")
+  .replace(/China Unicom.+?Network/, "CU")
+  .replace(/Chinanet/, "CT")
+  .replace("Amazon.com, Inc.", "AMZ")
+  .replace("Cloudflare, Inc.", "CF")
+  .replace(/(.+?)\s*((\ud83c[\udde6-\uddff]){2})\s*(.+)$/, "$2 $1 $4");
