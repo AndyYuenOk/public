5fe3be066cd6e5f82effd83ecf00d0b56cf537b5
Andy <2444909880@qq.com>
2026-04-28 16:35:08 +0800

    -

--- sub-store-rename-custom.js
+++ sub-store-rename-custom.js
@@ -1,9 +1,13 @@
 $server.name = $server.name
-  .replace(/China Mobile.+?corporation/, "CM")
-  .replace(/China Unicom(?:.+?Network)?/, "CU")
+  .replace(/China Mobile.*?(?= -)/, "CM")
+  .replace(/China Unicom.*?(?= -)/, "CU")
   .replace(/Chinanet/, "CT")
-  .replace("Amazon.com, Inc.", "AMZ")
-  .replace("Cloudflare, Inc.", "CF")
-  .replace("TECHNOLOGY CO., LIMITED", "")
+  .replace(/Amazon.*?(?= -)/, "AMZ")
+  .replace(/Cloudflare.*?(?= -)/, "CF")
+  .replace(/Microsoft.*?(?= -)/, "Azure")
+  .replace(/Data Communication.*?(?= -)/, "HiNet")
+  .replace(/HostPapa.*?(?= -)/, "HPAPA")
+  .replace(/NetLab.*?(?= -)/, "NetLab")
+  .replace(/PAN-LIAN.*?(?= -)/, "PL")
   .replace(/(.+?)\s*((\ud83c[\udde6-\uddff]){2})\s*(.+)$/, "$2 $1 $4")
   .replace("  ", " ");
