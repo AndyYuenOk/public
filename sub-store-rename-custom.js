0ba1abfa28a5f120dc4bf8d1ff04b25e282e0c20
Andy <2444909880@qq.com>
2026-05-06 13:07:57 +0800

    -

--- sub-store-rename-custom.js
+++ sub-store-rename-custom.js
@@ -66,7 +66,7 @@ function operator(proxies = [], targetPlatform, context) {
       }
       entrance = [
         proxy.entranceCountryCode,
-        proxy.entranceCountryCod == "CN" ? proxy.entranceRegion : "",
+        proxy.entranceCountryCode == "CN" ? proxy.entranceRegion : "",
         index,
         // $server.ipCity,
         normalizedIsp(
