effce27e80739636cb5a656582954373b20e8577
Andy <2444909880@qq.com>
2026-05-06 12:26:03 +0800

    -

--- sub-store-multiplier.js
+++ sub-store-multiplier.js
@@ -14,6 +14,6 @@ function operator(proxies, targetPlatform, context) {
     if (multiplierMap[multiplier].test(proxy.name)) {
       return multiplier < 1;
     }
-    return true;
+    return false;
   });
 }
