e1d019007bbd76f7afba37d50f4baa0d05c6bdc7
Andy <2444909880@qq.com>
2026-05-17 01:33:18 +0800

    -

--- sub-store-deduplicate.js
+++ sub-store-deduplicate.js
@@ -1,11 +1,22 @@
 function operator(proxies, targetPlatform, context) {
   const proxyMap = new Map();
   proxies.forEach((proxy) => {
-    let octets = $arguments.octets ?? 4;
     let key = proxy.name;
+
+    let octets = $arguments.octets ?? 4;
     if (proxy.egress.ip) {
       key = proxy.egress.ip.split(".").slice(0, octets).join(".");
     }
+
+    let egressKey =
+      proxy.egress.countryCode +
+      proxy.egress.region +
+      proxy.egress.city +
+      proxy.egress.asn;
+    if (/true|1/.test($arguments.geo ?? 0) && egressKey) {
+      key = egressKey;
+    }
+
     proxyMap.set(key, proxy);
   });
   return Array.from(proxyMap.values());
